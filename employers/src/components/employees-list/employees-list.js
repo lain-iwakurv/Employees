c99feea92d0a311062683755e3b879b5675e02b1
Altynai <altynai.kanatkyzy@inbox.ru>
2025-02-25 22:13:11 +0500

    first commit

---
+++ employers/src/components/employees-list/employees-list.js
@@ -0,0 +1,22 @@
+import EmployeesListItem from "../employees-list-item/employees-list-item"
+import './employees-list.css'
+
+const EmployeesList = ({data}) => {
+
+    const elements = data.map(item => {
+        const {id, ...itemProps} = item;
+        return (
+            <EmployeesListItem key={id} {...itemProps}/>
+            //<EmployeesListItem name={item.name} salary={item.salary} increase={item.increase}/>
+            //<EmployeesListItem {...item}/>
+        )
+    })
+
+    return (
+        <ul className="app-list list-group">
+            {elements}
+        </ul>
+    )
+}
+
+export default EmployeesList;
